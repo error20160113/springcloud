a475fd4b4d9109e1ea540e8800a9a7c2954abbee
error <error404gfw@gmail.com>
2018-07-09 17:00:11 +0800

    spring cloud

---
+++ service-feign/src/main/java/com/forezp/servicefeign/web/HiController.java
@@ -0,0 +1,25 @@
+package com.forezp.servicefeign.web;
+
+
+import com.forezp.servicefeign.service.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: zhaoy
+ * @Date: 2018/7/9 15:36
+ * @Description:
+ */
+@RestController
+public class HiController {
+
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+    @RequestMapping(value = "/hi",method = RequestMethod.GET)
+    public String sayHi(@RequestParam String name){
+        return schedualServiceHi.sayHiFromClientOne(name);
+    }
+}
